a355cdba0fd1c517c7bf0a2137048060c4dd38eb
Chris Grandin <chris.grandin@gmail.com>
2025-10-04 00:14:40 -0700

    Add directories to the system-wide PATH

---
+++ hake-precision-paths.sh
@@ -0,0 +1,5 @@
+PATH=":/usr/local/texlive/2025/bin/x86_64-linux:/usr/bin/admb/build/admb:/usr/bin/admb/build/admb/bin:/usr/bin/admb/contrib/ad2csv:/usr/bin/ss3/build:/usr/bin/gfiscam-dd:/usr/bin/gfiscam-dd/build/dist/bin:/usr/bin/gfiscam:/usr/bin/gfiscam/build/dist/bin:/usr/bin/ss3/build:/usr/bin/ss3_2024/build:/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games:/snap/bin:/opt/quarto/1.6.40/bin"
+MANPATH="/usr/local/texlive/2025/texmf-dist/doc/man"
+INFOPATH="/usr/local/texlive/2025/texmf-dist/doc/info"
+export DEEPL_AUTH_KEY="214ba7d1-0181-40ba-839d-4f7e9d931981:fx"
+
